fe509565d7c55f1fcaef1c7e2c4933fe62481dfc
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2024-12-28 04:29:27 +0100

    fix: fix deployment not working correctly when specifying NUXT_APP_BASE_URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  experimental: { appManifest: false },
   devtools: { enabled: true },
   modules: [ '@nuxt/fonts', ]
 })
